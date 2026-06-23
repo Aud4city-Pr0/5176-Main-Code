b1c4c3e8be79754724e1f597263cb2da4fd03248
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-06-23 17:27:02 -0400

    set driver controls to arcade drive with split controls.

--- src/main.cpp
+++ src/main.cpp
@@ -250,11 +250,7 @@ void opcontrol() {
     // TODO: remove later
     //print_rot_value(testRotation);
 
-    chassis.opcontrol_tank();  // Tank control
-    // chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
-    // chassis.opcontrol_arcade_standard(ez::SINGLE);  // Standard single arcade
-    // chassis.opcontrol_arcade_flipped(ez::SPLIT);    // Flipped split arcade
-    // chassis.opcontrol_arcade_flipped(ez::SINGLE);   // Flipped single arcade
+    chassis.opcontrol_arcade_standard(ez::SPLIT);   // Standard split arcade
 
     // . . .
     // Put more user control code here!
