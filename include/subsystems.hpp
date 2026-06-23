92c6d7dcc5463f11db04185e4349def8f029c791
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-06-23 19:58:53 -0400

    setup basics for implementing driver controlls

--- include/subsystems.hpp
+++ include/subsystems.hpp
@@ -3,11 +3,19 @@
 #include "EZ-Template/api.hpp"
 #include "api.h"
 #include "pros/rotation.hpp"
+#include "pros/motors.hpp"
+#include "mechanisums/intake.hpp"
 
 extern Drive chassis;
 
 // Your motors, sensors, etc. should go here.  Below are examples
 
-// inline pros::Motor intake(1);
-// inline pros::adi::DigitalIn limit_switch('A');
+// sensors
 inline pros::Rotation testRotation(15);
+
+// motors
+inline pros::Motor intakeA(8);
+inline pros::Motor intakeB(9);
+
+// mechansiums
+inline IntakeClass::intake botIntake(intakeA, intakeB, 10000);
--- src/helpers.cpp
+++ src/helpers.cpp
@@ -1,8 +1,4 @@
-
 #include "main.h"
-#include "pros/rotation.hpp"
-#include "pros/screen.h"
-#include "helpers.hpp"
 //-----------------------------------------
 // File name: helpers.cpp
 // Creation date: 6/12/26
@@ -10,6 +6,15 @@
 // Description: Contains some helper functions for the robot
 //-----------------------------------------
 
+// includes
+//-----------
+#include "pros/rotation.hpp"
+#include "pros/screen.h"
+#include "helpers.hpp"
+#include "pros/motors.hpp"
+#include "mechanisums/intake.hpp"
+
+// conversion functions
 void print_rot_value(pros::Rotation rotSensor) {
     int converted_val = rotSensor.get_angle() / 100;
     if(converted_val == 360) {
@@ -17,4 +22,11 @@ void print_rot_value(pros::Rotation rotSensor) {
         rotSensor.reset_position();
     }
     pros::screen::print(pros::E_TEXT_MEDIUM, 3, "Current rotation %d", converted_val);
+}
+
+// helper functions for driver and auto
+
+// contains 
+void driver_control_intake() {
+    // code
 }
