a8bac259568f0743cf63f6709aed9616b7a4721e
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-06-23 19:31:04 -0400

    added intake mech classes

---
+++ include/mechanisums/intake.hpp
@@ -0,0 +1,45 @@
+#pragma once
+//-----------------------------------------
+// File name: intake.hpp
+// Creation date: 6/23/26
+// Created By: Zach D
+// Description: The intake mechanism class, contains methods for controling intake
+//-----------------------------------------
+
+// includes
+//-------------
+#include "pros/motors.hpp"
+
+// intake class
+namespace IntakeClass {
+
+    // intake direction enum
+    enum Direction {
+        FORWARD = 0,
+        BACKAWRD = 1
+    };
+
+    class intake {
+        // interal class variables or functions that are not messed with by other classes or code in main
+        private:
+        int defaultSpeed = 0;
+        pros::Motor* motorA;
+        pros::Motor* motorB;
+
+        // external variables or functions that can be used or changed by other classes or code in main
+        public:
+        // initalizer/constructor function that creates a intake object by taking in two motors and a default speed
+        intake(pros::Motor &inkateMotorA, pros::Motor &intakeMotorB, int intakeDefaultSpeed) {
+            motorA = &inkateMotorA;
+            motorB = &intakeMotorB;
+            defaultSpeed = intakeDefaultSpeed;
+        }
+
+        // void function that turns intake on and off using a boolean true/false value
+        void set_status(bool intakeStatus);
+        // variation of set_status that overrides the defaultSpeed variable to allow for a custom speed that ranges between -12000 and 12000 millivolts
+        void set_status(bool intakeStatus, int speed);
+        // setter function that controlls direction of intake using enums
+        void set_intake_direction(Direction intakeDir);
+    };
+};
---
+++ src/mechanisums/intake.cpp
@@ -0,0 +1,43 @@
+//-----------------------------------------
+// File name: intake.cpp
+// Creation date: 6/23/26
+// Created By: Zach D
+// Description: The intake mechanism class, contains methods for controling intake
+//-----------------------------------------
+
+// includes
+//--------------
+#include "mechanisums/intake.hpp"
+#include "pros/motors.hpp"
+
+
+// the class helpers function code
+void IntakeClass::intake::set_status(bool intakeStatus) {
+    if(intakeStatus == true) {
+        IntakeClass::intake::motorA->move_voltage(IntakeClass::intake::defaultSpeed);
+        IntakeClass::intake::motorB->move_voltage(IntakeClass::intake::defaultSpeed);
+    } else if(intakeStatus == false) {
+        IntakeClass::intake::motorA->brake();
+        IntakeClass::intake::motorB->brake();
+    }
+}
+
+void IntakeClass::intake::set_status(bool intakeStatus, int speed) {
+    if(intakeStatus == true) {
+        IntakeClass::intake::motorA->move_voltage(speed);
+        IntakeClass::intake::motorB->move_voltage(speed);
+    } else if(intakeStatus == false) {
+        IntakeClass::intake::motorA->brake();
+        IntakeClass::intake::motorB->brake();
+    }
+}
+
+void IntakeClass::intake::set_intake_direction(IntakeClass::Direction intakeDir) {
+    if(intakeDir == IntakeClass::FORWARD) {
+        IntakeClass::intake::motorA->set_reversed(false);
+        IntakeClass::intake::motorB->set_reversed(false);
+    } else if (intakeDir == IntakeClass::BACKAWRD) {
+        IntakeClass::intake::motorA->set_reversed(true);
+        IntakeClass::intake::motorB->set_reversed(true);
+    }
+}
