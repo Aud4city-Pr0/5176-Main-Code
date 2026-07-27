b5e95d356a6660c2924c6a4811596a0d57779a4c
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-07-27 15:37:33 -0400

    Revert "added sensor reset to init"
    This reverts commit 72084dfbc87bc469d2597f4850a84f55f1583e40.

--- src/mechanisums/clawarm.cpp
+++ src/mechanisums/clawarm.cpp
@@ -53,7 +53,6 @@ void ArmClass::arm::update_pid() {
 // this function inits the sensors, pid and motor
 void ArmClass::arm::initalize() {
     armMotor->set_brake_mode(pros::v5::MotorBrake::hold);
-    armRotation->reset_position();
     armMotor->tare_position();
     ArmPID.exit_condition_set(5, 10);
 }
