fd8fe3d6395445d28ff83f86402601f99a0e69ce
Aud4city-Pr0 <zachary.duriancik@gmail.com>
2026-07-28 16:11:32 -0400

    fixed driver controll code for claw

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -88,7 +88,7 @@ void driver_control_arm() {
 void driver_controll_claw() {
     if(master.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
         // incrementing by 1
-        
+        postionStateClaw++;
         // preventing state from going out of array
         if(postionStateClaw >= MAX_STATE_CLAW) {
             postionStateClaw = 0;
